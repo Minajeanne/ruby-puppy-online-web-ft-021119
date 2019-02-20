14e1305018f5661b6102c38688416fdc39c8e63a
Alicia Santiago <albrown07@mac.com>
2019-02-20 20:40:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,16 @@ class Dog
     @@all << self 
   end
   
-  def 
+  def self.all
+    @@all.each do |dog|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end 
   
   def self.clear_all
     clear_all.clear 
